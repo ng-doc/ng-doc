29236532569bff966c8e812d5378893a426af911
Alexander Skoropad <alex@skoropad.dev>
2022-05-26 01:31:39 +0200

    chore(project): add linting

--- libs/builder/src/renderer.ts
+++ libs/builder/src/renderer.ts
@@ -1,33 +1,30 @@
-import * as ejs from 'ejs';
 import * as path from 'path';
 import * as fs from 'fs';
+import * as ejs from 'ejs';
 import {TEMPLATES_PATH} from './variables';
-import {NgDocRendererOptions} from './interfaces/renderer-options';
+import {NgDocRendererOptions} from './interfaces';
 
 export class NgDocRenderer<T = any> {
-  constructor(
-    private readonly context?: T
-  ) {
-  }
+    constructor(private readonly context?: T) {}
 
-  async render(template: string, options?: NgDocRendererOptions<T>): Promise<string> {
-    const templatePath: string = options?.fullPath ? template : path.join(TEMPLATES_PATH, template);
+    async render(template: string, options?: NgDocRendererOptions<T>): Promise<string> {
+        const templatePath: string = options?.fullPath ? template : path.join(TEMPLATES_PATH, template);
 
-    return await ejs.renderFile(templatePath, options?.overrideContext ?? this.context);
-  }
+        return await ejs.renderFile(templatePath, options?.overrideContext ?? this.context);
+    }
 
-  async renderToFile(template: string, filePath: string, options?: NgDocRendererOptions<T>): Promise<void> {
-    const rendered: string = await this.render(template, options);
+    async renderToFile(template: string, filePath: string, options?: NgDocRendererOptions<T>): Promise<void> {
+        const rendered: string = await this.render(template, options);
 
-    fs.mkdirSync(path.dirname(filePath), {recursive: true});
-    fs.writeFileSync(filePath, rendered);
-  }
+        fs.mkdirSync(path.dirname(filePath), {recursive: true});
+        fs.writeFileSync(filePath, rendered);
+    }
 
-  async renderToFolder(template: string, folderPath: string, options?: NgDocRendererOptions<T>): Promise<void> {
-    const rendered: string = await this.render(template, options);
-    const fileName: string = path.basename(template).replace(/\.ejs$/, '');
+    async renderToFolder(template: string, folderPath: string, options?: NgDocRendererOptions<T>): Promise<void> {
+        const rendered: string = await this.render(template, options);
+        const fileName: string = path.basename(template).replace(/\.ejs$/, '');
 
-    fs.mkdirSync(path.dirname(folderPath), {recursive: true});
-    fs.writeFileSync(path.join(folderPath, fileName), rendered);
-  }
+        fs.mkdirSync(path.dirname(folderPath), {recursive: true});
+        fs.writeFileSync(path.join(folderPath, fileName), rendered);
+    }
 }
