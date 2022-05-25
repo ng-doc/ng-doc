5aed89a38fab84168deaf395e9a30beda0281851
Alexander Skoropad <alex@skoropad.dev>
2022-05-26 01:14:46 +0200

    chore(project): migrate to nx monorepo

--- apps/documentation/src/app/app.component.ts
+++ apps/documentation/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'nx-ng-doc-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'nx-ng-doc-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'documentation';
+    title = 'documentation';
 }
