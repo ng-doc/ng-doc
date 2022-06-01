ca545f6f081943badb10e7fc92b622edcd8c70a5
Alexander Skoropad <alex@skoropad.dev>
2022-06-01 23:23:12 +0200

    chore(project): developing

--- libs/app/directives/demo-renderer/demo-renderer.module.ts
+++ libs/app/directives/demo-renderer/demo-renderer.module.ts
@@ -1,4 +1,5 @@
 import {NgModule} from '@angular/core';
+
 import {NgDocDemoRendererDirective} from './demo-renderer.directive';
 
 @NgModule({
--- libs/app/directives/markdown/markdown.module.ts
+++ libs/app/directives/markdown/markdown.module.ts
@@ -1,4 +1,5 @@
 import {NgModule} from '@angular/core';
+
 import {NgDocMarkdownDirective} from './markdown.directive';
 
 @NgModule({
