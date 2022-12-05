abf01fab2961eb1c2be46607dd0a3526081f1141
Alexander Skoropad <alex@skoropad.dev>
2022-12-05 23:57:17 +0100

    chore(project): developing

---
+++ apps/documentation/src/app/customization/ng-doc.category.ts
@@ -0,0 +1,9 @@
+import {NgDocCategory} from '@ng-doc/builder';
+
+const CustomizationCategory: NgDocCategory = {
+	title: 'Customization',
+	order: 4,
+	expandable: true,
+}
+
+export default CustomizationCategory;
---
+++ apps/documentation/src/app/customization/themes/ng-doc.page.ts
@@ -0,0 +1,12 @@
+import {NgDocPage} from '@ng-doc/builder';
+import CustomizationCategory from '../ng-doc.category';
+
+const ThemesPage: NgDocPage = {
+	title: 'Themes',
+	mdFile: './index.md',
+	category: CustomizationCategory,
+	order: 1,
+	keyword: 'CustomizationThemes',
+}
+
+export default ThemesPage;
