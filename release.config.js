b8848e9de4d2054ad2bf051b593c7ce94879bb8a
Alexander Skoropad <alex@skoropad.dev>
2022-12-08 17:34:00 +0100

    chore(project): developing

--- release.config.js
+++ release.config.js
@@ -1,67 +1,16 @@
 module.exports = {
 	branches: ['+([0-9])?(.{+([0-9]),x}).x', 'release', 'beta'],
 	plugins: [
-		...require('@emias-kpi/linters/semantic-release/base.release.config.js').plugins,
+		"@semantic-release/commit-analyzer",
+		"@semantic-release/release-notes-generator",
+		"@semantic-release/changelog",
 		[
-			'@emias-kpi/semantic-release-npm',
+			"@semantic-release/git",
 			{
-				packages: [
-					{
-						pkgRoot: 'dist/solit/lucidus',
-					},
-					{
-						pkgRoot: 'dist/solit/lucidus-cdk',
-					},
-					{
-						pkgRoot: 'dist/solit/lucidus-core',
-					},
-					{
-						pkgRoot: 'dist/solit/lucidus-date-fns-adapter',
-					},
-					{
-						pkgRoot: 'dist/solit/lucidus-moment-adapter',
-					},
-					{
-						pkgRoot: 'dist/solit/lucidus-luxon-adapter',
-					},
-				],
-			},
+				assets: ["CHANGELOG.md", "package.json"],
+				message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+			}
 		],
-		[
-			'./plugins/semantic-release/update-dependencies.js',
-			{
-				packages: [
-					'dist/solit/lucidus',
-					'dist/solit/lucidus-cdk',
-					'dist/solit/lucidus-core',
-					'dist/solit/lucidus-date-fns-adapter',
-					'dist/solit/lucidus-moment-adapter',
-					'dist/solit/lucidus-luxon-adapter',
-				],
-			},
-		],
-		[
-			'semantic-release-telegram-bot',
-			{
-				success: {
-					message: '${nextRelease.notes}',
-				},
-				notifications: [
-					{
-						chatIds: '408225585',
-						notifyOnFail: true,
-						notifyOnSuccess: false,
-					},
-					{
-						chatIds: '408225585',
-						branch: 'rc/*',
-					},
-					{
-						chatIds: '-1001149567831',
-						branch: 'release/*',
-					},
-				],
-			},
-		],
+		"@semantic-release/github"
 	],
 };
