af7908b503c069f5b000b5a3fcd826ea025d3e7e
Alexander Skoropad <alex@skoropad.dev>
2022-12-14 16:34:15 +0100

    test(project): fix tests

--- apps/ng-doc/jest.config.ts
+++ apps/ng-doc/jest.config.ts
@@ -1,22 +1,22 @@
 /* eslint-disable */
 export default {
-  displayName: 'ng-doc',
-  preset: '../../jest.preset.js',
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(HTML|svg)$',
-    },
-  },
-  coverageDirectory: '../../coverage/apps/ng-doc',
-  transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
-  },
-  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
-  snapshotSerializers: [
-    'jest-preset-angular/build/serializers/no-ng-attributes',
-    'jest-preset-angular/build/serializers/ng-snapshot',
-    'jest-preset-angular/build/serializers/HTML-comment',
-  ],
+	displayName: 'ng-doc',
+	preset: '../../jest.preset.js',
+	setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+	globals: {
+		'ts-jest': {
+			tsconfig: '<rootDir>/tsconfig.spec.json',
+			stringifyContentPathRegex: '\\.(HTML|svg)$',
+		},
+	},
+	coverageDirectory: '../../coverage/apps/ng-doc',
+	transform: {
+		'^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+	},
+	transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+	snapshotSerializers: [
+		'jest-preset-angular/build/serializers/no-ng-attributes',
+		'jest-preset-angular/build/serializers/ng-snapshot',
+		'jest-preset-angular/build/serializers/HTML-comment',
+	],
 };
