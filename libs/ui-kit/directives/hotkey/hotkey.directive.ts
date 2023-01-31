e931994febd1fbdb520238bd88945336950662d5
Alexander Skoropad <alex@skoropad.dev>
2023-01-31 21:59:49 +0100

    feat(ui-kit): add `NgDocHotkeyDirective` for binding shortcuts

---
+++ libs/ui-kit/directives/hotkey/hotkey.directive.ts
@@ -0,0 +1,48 @@
+import {DOCUMENT} from '@angular/common';
+import {Directive, EventEmitter, Inject, Input, NgZone, Output} from '@angular/core';
+import {isKeyboardEvent, objectKeys} from '@ng-doc/core';
+import {ngDocZoneOptimize} from '@ng-doc/ui-kit/observables';
+import {UntilDestroy, untilDestroyed} from '@ngneat/until-destroy';
+import {Document} from 'postcss';
+import {fromEvent, Observable} from 'rxjs';
+import {filter, share} from 'rxjs/operators';
+
+const KEYUP_EVENT: Observable<Event> = fromEvent(document, 'keyup').pipe(share());
+
+@Directive({
+	selector: '[ngDocHotkey]',
+})
+@UntilDestroy()
+export class NgDocHotkeyDirective {
+	@Input('ngDocHotkey')
+	hotkey?: Partial<KeyboardEvent>;
+
+	@Output('ngDocHotkey')
+	callback: EventEmitter<void> = new EventEmitter<void>();
+
+	constructor(
+		@Inject(DOCUMENT)
+		private readonly document: Document,
+		private readonly ngZone: NgZone,
+	) {
+		KEYUP_EVENT.pipe(
+			filter(isKeyboardEvent),
+			filter((event: KeyboardEvent) =>
+				objectKeys(this.hotkey ?? {}).every(
+					(key: keyof KeyboardEvent) => this.hotkey && this.hotkey[key] === event[key],
+				),
+			),
+			filter((event: KeyboardEvent) => {
+				if (event.target instanceof HTMLElement) {
+					return !['input', 'textarea', 'select'].includes(event.target.tagName.toLowerCase());
+				}
+				return true;
+			}),
+			ngDocZoneOptimize(this.ngZone),
+			untilDestroyed(this),
+		).subscribe((event: KeyboardEvent) => {
+			event.preventDefault();
+			this.callback.emit();
+		});
+	}
+}
