1d61841baf6e2f1c4af8f10165490e019a5fe9fa
Alexander Skoropad <alex@skoropad.dev>
2023-02-09 19:28:17 +0100

    fix(sidebar): sidebar doesn't block global scroll

--- libs/app/services/sidebar/sidebar.service.ts
+++ libs/app/services/sidebar/sidebar.service.ts
@@ -1,5 +1,6 @@
 import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
-import {Injectable} from '@angular/core';
+import {DOCUMENT} from '@angular/common';
+import {Inject, Injectable} from '@angular/core';
 import {Event, NavigationEnd, Router} from '@angular/router';
 import {UntilDestroy, untilDestroyed} from '@ngneat/until-destroy';
 import {BehaviorSubject, Observable, of} from 'rxjs';
@@ -14,7 +15,12 @@ export class NgDocSidebarService {
 	protected readonly observer: Observable<boolean>;
 	protected readonly visible: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
-	constructor(protected readonly breakpointObserver: BreakpointObserver, protected readonly router: Router) {
+	constructor(
+		@Inject(DOCUMENT)
+		protected readonly document: Document,
+		protected readonly breakpointObserver: BreakpointObserver,
+		protected readonly router: Router,
+	) {
 		this.observer = this.breakpointObserver.observe(this.breakpoints).pipe(pluck('matches'), untilDestroyed(this));
 
 		this.router.events
@@ -40,11 +46,17 @@ export class NgDocSidebarService {
 	}
 
 	show(): void {
-		!this.visible.value && this.visible.next(true);
+		if (!this.visible.value) {
+			this.visible.next(true);
+			this.document.documentElement.classList.add('cdk-global-scrollblock');
+		}
 	}
 
 	hide(): void {
-		this.visible.value && this.visible.next(false);
+		if (this.visible.value) {
+			this.visible.next(false);
+			this.document.documentElement.classList.remove('cdk-global-scrollblock');
+		}
 	}
 
 	toggle(): void {
