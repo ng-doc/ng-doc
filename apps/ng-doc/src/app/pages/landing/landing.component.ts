5f4afdaa595f2512af3c20f8da8820de20b82317
Alexander Skoropad <alex@skoropad.dev>
2023-02-21 22:53:36 +0100

    docs(docs): add landing page

--- apps/ng-doc/src/app/pages/landing/landing.component.ts
+++ apps/ng-doc/src/app/pages/landing/landing.component.ts
@@ -3,7 +3,7 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 @Component({
 	selector: 'ng-doc-landing',
 	templateUrl: './landing.component.html',
-	styleUrls: ['./landing.component.css'],
+	styleUrls: ['./landing.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LandingComponent {}
