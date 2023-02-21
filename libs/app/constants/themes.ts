a26da1ced10ce324e581882d154172d09f478d82
Alexander Skoropad <alex@skoropad.dev>
2023-02-21 13:30:43 +0100

    feat(app): add dark purple theme

--- libs/app/constants/themes.ts
+++ libs/app/constants/themes.ts
@@ -1,3 +1,7 @@
 import {NgDocTheme} from '@ng-doc/app/interfaces';
 
 export const NG_DOC_NIGHT_THEME: NgDocTheme = {id: 'ng-doc-night', path: 'assets/ng-doc/app/themes/ng-doc-night.css'};
+export const NG_DOC_DARK_PURPLE: NgDocTheme = {
+	id: 'ng-doc-dark-purple',
+	path: 'assets/ng-doc/app/themes/ng-doc-dark-purple.css',
+};
