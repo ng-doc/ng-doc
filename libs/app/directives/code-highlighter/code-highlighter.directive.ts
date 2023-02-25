ab93672e0d5e07aa4e378054929d9974253a2dbf
Alexander Skoropad <alex@skoropad.dev>
2023-02-25 23:16:51 +0100

    perf(app): remove unused languages imports for highlight.js

--- libs/app/directives/code-highlighter/code-highlighter.directive.ts
+++ libs/app/directives/code-highlighter/code-highlighter.directive.ts
@@ -1,26 +1,8 @@
 import {Directive, ElementRef, HostBinding, Input, OnChanges} from '@angular/core';
 import {HighlightResult} from 'highlight.js';
 import highlight from 'highlight.js/lib/core';
-import bash from 'highlight.js/lib/languages/bash';
-import css from 'highlight.js/lib/languages/css';
-import javascript from 'highlight.js/lib/languages/javascript';
-import json from 'highlight.js/lib/languages/json';
-import less from 'highlight.js/lib/languages/less';
-import markdown from 'highlight.js/lib/languages/markdown';
-import scss from 'highlight.js/lib/languages/scss';
-import twig from 'highlight.js/lib/languages/twig';
-import typescript from 'highlight.js/lib/languages/typescript';
 import xml from 'highlight.js/lib/languages/xml';
 
-highlight.registerLanguage('javascript', javascript);
-highlight.registerLanguage('typescript', typescript);
-highlight.registerLanguage('css', css);
-highlight.registerLanguage('less', less);
-highlight.registerLanguage('scss', scss);
-highlight.registerLanguage('json', json);
-highlight.registerLanguage('bash', bash);
-highlight.registerLanguage('markdown', markdown);
-highlight.registerLanguage('twig', twig);
 highlight.registerLanguage('html', xml);
 highlight.registerLanguage('xml', xml);
 
