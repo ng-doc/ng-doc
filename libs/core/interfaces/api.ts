5f52ab8006876434a2e3133e07e688283e68964e
Alexander Skoropad <alex@skoropad.dev>
2023-03-25 14:24:36 +0100

    feat(app): conditionally remove docs page from sidebar
    Closes #35

--- libs/core/interfaces/api.ts
+++ libs/core/interfaces/api.ts
@@ -1,15 +1,10 @@
 import {NgDocApiScope} from './api-scope';
+import {NgDocBaseEntity} from './base-entity';
 import {NgDocCategory} from './category';
 
-export interface NgDocApi {
-	/** The page title */
-	title: string;
+export interface NgDocApi extends NgDocBaseEntity {
 	/** The API scopes, you can use it to define scopes for files that should be included to the API */
 	scopes: NgDocApiScope[];
 	/** The page category */
 	category?: NgDocCategory;
-	/** The route of the page (current sourceFileFolder title by default) */
-	route?: string;
-	/** Order is using for sorting pages and categories in sidebar */
-	order?: number;
 }
