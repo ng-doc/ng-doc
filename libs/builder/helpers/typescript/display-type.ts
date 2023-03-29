f1581fdbee7393c0d12778a941337ab02058cb69
Alexander Skoropad <alex@skoropad.dev>
2023-03-29 22:02:52 +0200

    fix(builder): ng-doc truncates long literal types

--- libs/builder/helpers/typescript/display-type.ts
+++ libs/builder/helpers/typescript/display-type.ts
@@ -5,5 +5,5 @@ import {Type, TypeFormatFlags} from 'ts-morph';
  * @param type
  */
 export function displayType(type: Type): string {
-	return type.getText(undefined, TypeFormatFlags.None);
+	return type.getText(undefined, TypeFormatFlags.NoTruncation);
 }
