5ca03a1cbc9615fced9427fea91daa0d60798309
Alexander Skoropad <alex@skoropad.dev>
2023-04-01 01:33:06 +0200

    fix(project): dependency injection error after update to 15.10.0

--- libs/app/components/breadcrumb/breadcrumb.component.ts
+++ libs/app/components/breadcrumb/breadcrumb.component.ts
@@ -1,5 +1,5 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {ActivatedRoute, Router, UrlSegment} from '@angular/router';
+import {ActivatedRoute} from '@angular/router';
 import {NgDocRootPage} from '@ng-doc/app/classes';
 import {isPresent} from '@ng-doc/core/helpers/is-present';
 
@@ -11,9 +11,10 @@ import {isPresent} from '@ng-doc/core/helpers/is-present';
 })
 export class NgDocBreadcrumbComponent {
 	breadcrumbs: string[] = [];
-	home: string = this.rootPage.routePrefix || '/';
+	home: string;
 	constructor(private readonly rootPage: NgDocRootPage, private readonly route: ActivatedRoute) {
-		this.breadcrumbs =this.route.pathFromRoot
+		this.home = this.rootPage.routePrefix || '/';
+		this.breadcrumbs = this.route.pathFromRoot
 			.filter((route: ActivatedRoute) => !route.snapshot.url.length)
 			.map((route: ActivatedRoute) => route.snapshot.title)
 			.filter(isPresent);
