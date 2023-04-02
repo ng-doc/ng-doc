c873ac5ea811bca4da2e53aae138ddf106a72e6a
Alexander Skoropad <alex@skoropad.dev>
2023-04-03 00:30:38 +0200

    test(builder): add tests for `codeTypeFromExt` function

---
+++ libs/builder/helpers/testing/code-type-from-ext.spec.ts
@@ -0,0 +1,31 @@
+import {codeTypeFromExt} from '../code-type-from-ext';
+
+describe('codeTypeFromExt', () => {
+	it('should return TypeScript for ts files', () => {
+		expect(codeTypeFromExt('test.ts')).toBe('TypeScript');
+	});
+
+	it('should return HTML for html files', () => {
+		expect(codeTypeFromExt('test.html')).toBe('HTML');
+	});
+
+	it('should return CSS for css files', () => {
+		expect(codeTypeFromExt('test.css')).toBe('CSS');
+	});
+
+	it('should return SCSS for scss files', () => {
+		expect(codeTypeFromExt('test.scss')).toBe('SCSS');
+	});
+
+	it('should return LESS for less files', () => {
+		expect(codeTypeFromExt('test.less')).toBe('LESS');
+	});
+
+	it('should return SASS for sass files', () => {
+		expect(codeTypeFromExt('test.sass')).toBe('SASS');
+	});
+
+	it('should return unknown for unknown files', () => {
+		expect(codeTypeFromExt('test.pdf')).toBe('unknown');
+	})
+});
