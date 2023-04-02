5d1b1d045e6d4cef092aab6354bb98eee1d538b6
Alexander Skoropad <alex@skoropad.dev>
2023-04-02 16:33:48 +0200

    test(builder): add tests for `filterByStatic` function

---
+++ libs/builder/helpers/typescript/testing/filter-by-static.spec.ts
@@ -0,0 +1,85 @@
+import {ClassDeclaration, MethodDeclaration, Project, PropertyDeclaration, Scope, SourceFile} from 'ts-morph';
+
+import {createProject} from '../create-project';
+import {filterByScope} from '../filter-by-scope';
+import {filterByStatic} from '../filter-by-static';
+
+describe('filterByScope', () => {
+	let project: Project;
+
+	beforeEach(() => {
+		project = createProject({useInMemoryFileSystem: true});
+	});
+
+	it('should return static property', () => {
+		const sourceFile: SourceFile = project.createSourceFile(
+			'class.ts',
+			`
+				class Test {
+					static property1: string;
+					property2: string;
+				}
+			`,
+		);
+		const declaration: ClassDeclaration = sourceFile.getClassOrThrow('Test');
+
+		expect(
+			filterByStatic(declaration.getProperties(), true).map((property: PropertyDeclaration) =>
+				property.getName(),
+			),
+		).toStrictEqual(['property1']);
+	});
+
+	it('should return non-static property', () => {
+		const sourceFile: SourceFile = project.createSourceFile(
+			'class.ts',
+			`
+				class Test {
+					static property1: string;
+					property2: string;
+				}
+			`,
+		);
+		const declaration: ClassDeclaration = sourceFile.getClassOrThrow('Test');
+
+		expect(
+			filterByStatic(declaration.getProperties(), false).map((property: PropertyDeclaration) =>
+				property.getName(),
+			),
+		).toStrictEqual(['property2']);
+	})
+
+	it('should return static method', () => {
+		const sourceFile: SourceFile = project.createSourceFile(
+			'class.ts',
+			`
+				class Test {
+					static method1() {}
+					method2() {}
+				}
+			`,
+		);
+		const declaration: ClassDeclaration = sourceFile.getClassOrThrow('Test');
+
+		expect(
+			filterByStatic(declaration.getMethods(), true).map((method: MethodDeclaration) => method.getName()),
+		).toStrictEqual(['method1']);
+	})
+
+	it('should return non-static method', () => {
+		const sourceFile: SourceFile = project.createSourceFile(
+			'class.ts',
+			`
+				class Test {
+					static method1() {}
+					method2() {}
+				}
+			`,
+		);
+		const declaration: ClassDeclaration = sourceFile.getClassOrThrow('Test');
+
+		expect(
+			filterByStatic(declaration.getMethods(), false).map((method: MethodDeclaration) => method.getName()),
+		).toStrictEqual(['method2']);
+	})
+});
