7523fcbaedef247fff06bed95ea9981e1e4e135a
Alex Skoropad <alex@skoropad.dev>
2023-06-04 14:59:33 +0200

    refactor(builder): add api config

--- apps/ng-doc/src/app/pages/docs/ng-doc.api.ts
+++ apps/ng-doc/src/app/pages/docs/ng-doc.api.ts
@@ -2,7 +2,28 @@ import {NgDocApi} from '@ng-doc/core';
 
 export const api: NgDocApi = {
   title: 'API References',
-  scopes: [],
+  scopes: [
+    {
+      name: '@ng-doc/app',
+      route: 'app',
+      include: 'libs/app/**/*.ts',
+    },
+    {
+      name: '@ng-doc/builder',
+      route: 'builder',
+      include: ['libs/builder/interfaces/**.ts', 'libs/builder/types/**.ts'],
+    },
+    {
+      name: '@ng-doc/ui-kit',
+      route: 'ui-kit',
+      include: 'libs/ui-kit/**/*.ts',
+    },
+    {
+      name: '@ng-doc/core',
+      route: 'core',
+      include: 'libs/core/**/*.ts',
+    },
+  ],
 };
 
 export default api;
