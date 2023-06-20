9c72b0bf35d10a4ad7e539a87823fcef7340a079
a.skoropad <a.skoropad@fourthline.com>
2023-06-20 17:00:27 +0200

    fix(keywords-loaders): remove warning about experimental fetch method

--- libs/keywords-loaders/angular/helpers/fetch-docs.ts
+++ libs/keywords-loaders/angular/helpers/fetch-docs.ts
@@ -1,6 +1,7 @@
 import {NgResponse} from '../interfaces';
 import {NgVersion} from '../types';
 import {getNgHost} from './get-ng-host';
+import fetch, {Response} from 'node-fetch';
 
 /**
  *
@@ -8,6 +9,6 @@ import {getNgHost} from './get-ng-host';
  */
 export async function fetchDocs(version?: NgVersion): Promise<NgResponse> {
 	return await fetch(`${getNgHost(version)}/generated/docs/app/search-data.json`).then((response: Response) =>
-		response.json(),
+		response.json() as Promise<NgResponse>,
 	);
 }
