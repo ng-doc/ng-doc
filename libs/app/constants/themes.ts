fb9cffe01f3f491c24717cc39175a9356effa2c8
Alex Skoropad <alex@skoropad.dev>
2023-09-01 15:27:00 +0200

    fix(app): dark-purple theme was removed
    BREAKING CHANGE: `dark-purple` theme was removed

--- libs/app/constants/themes.ts
+++ libs/app/constants/themes.ts
@@ -35,39 +35,3 @@ export const NG_DOC_NIGHT_THEME: NgDocTheme = {
 	id: 'ng-doc-night',
 	path: 'assets/ng-doc/app/themes/ng-doc-night.css',
 };
-
-/**
- * Dark purple theme. You can set it manually by calling `NgDocThemeService.set` method.
- *
- * ```typescript
- * import {Component} from '@angular/core';
- * import {NgDocThemeService, NG_DOC_DARK_PURPLE_THEME} from '@ng-doc/app';
- *
- * @Component()
- * export class AppComponent {
- * 	constructor(
- * 	    private readonly themeService: NgDocThemeService,
- * 	) {
- * 	    this.themeService.setTheme(NG_DOC_DARK_PURPLE_THEME.id);
- * 	}
- * }
- * ```
- *
- * Or you can set it as default theme in your app module:
- * ```typescript
- * import {NgModule} from '@angular/core';
- * import {NgDocModule, NG_DOC_DARK_PURPLE_THEME} from '@ng-doc/app';
- * import {AppComponent} from './app.component';
- *
- * @NgModule({
- * 	declarations: [AppComponent],
- * 	imports: [NgDocModule.forRoot({defaultTheme: NG_DOC_DARK_PURPLE_THEME.id})],
- * 	bootstrap: [AppComponent],
- * })
- * export class AppModule {}
- * ```
- */
-export const NG_DOC_DARK_PURPLE_THEME: NgDocTheme = {
-	id: 'ng-doc-dark-purple',
-	path: 'assets/ng-doc/app/themes/ng-doc-dark-purple.css',
-};
