25ca35d7d1677a6c40352d5f35ee8011a6bf1d2b
Alex Skoropad <alex@skoropad.dev>
2024-03-18 19:24:54 +0100

    chore(builder): saving progress

--- apps/ng-doc/poc/ng-doc.api.ts
+++ apps/ng-doc/poc/ng-doc.api.ts
@@ -2,7 +2,13 @@ import { NgDocApi } from '@ng-doc/core';
 
 const api: NgDocApi = {
   title: 'API References',
-  scopes: [],
+  scopes: [
+    {
+      name: '@ng-doc/core',
+      route: 'core',
+      include: 'libs/core/**/*.ts',
+    },
+  ],
 };
 
 export default api;
