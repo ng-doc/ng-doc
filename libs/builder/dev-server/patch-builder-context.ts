68a903978db98ae5181b3b2891e3b4b3c2728159
Char2s <Char2s@outlook.com>
2024-05-01 03:13:38 -0400

    fix(builder): eliminate dev-server schema validation error (#153)
    Closes #147

---
+++ libs/builder/dev-server/patch-builder-context.ts
@@ -0,0 +1,36 @@
+import { BuilderContext } from '@angular-devkit/architect';
+
+// The DevServer uses EsBuild only for specific builder names.
+// https://github.com/just-jeb/angular-builders/blob/dbf4d281a29c2f93d309bba0e155c3a22965130c/packages/custom-esbuild/src/dev-server/patch-builder-context.ts#L14-L47
+/**
+ *
+ * @param context
+ * @param config
+ * @param config.mock
+ * @param config.with
+ * @param config.optionsTransform
+ */
+export function patchBuilderContext(
+	context: BuilderContext,
+	config: {
+		mock: string[];
+		with: string;
+		optionsTransform: (options: any) => void;
+	},
+): BuilderContext {
+	const getBuilderNameForTarget: typeof context.getBuilderNameForTarget = async (target) => {
+		const builderName = await context.getBuilderNameForTarget(target);
+		if (!config.mock.includes(builderName)) return builderName;
+		return config.with;
+	};
+
+	const getTargetOptions: typeof context.getTargetOptions = async (target) => {
+		const builderName = await context.getBuilderNameForTarget(target);
+		if (!config.mock.includes(builderName)) return context.getTargetOptions(target);
+		const options = await context.getTargetOptions(target);
+		config.optionsTransform(options);
+		return options;
+	};
+
+	return { ...context, getBuilderNameForTarget, getTargetOptions };
+}
