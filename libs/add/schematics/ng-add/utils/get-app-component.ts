d21ffee5452bc63795573908a7d42cb185428cf1
Aleksandr Skoropad <alex@skoropad.dev>
2024-12-08 17:12:29 +0100

    fix(project): ng-morph imports

--- libs/add/schematics/ng-add/utils/get-app-component.ts
+++ libs/add/schematics/ng-add/utils/get-app-component.ts
@@ -1,10 +1,10 @@
 import { Tree } from '@angular-devkit/schematics';
 import {
-	ClassDeclaration,
-	Expression,
-	getBootstrapApplicationFn,
-	getMainModule,
-	Node,
+  ClassDeclaration,
+  Expression,
+  getBootstrapApplicationFn,
+  getMainModule,
+  Node,
 } from 'ng-morph';
 
 import { getInitializer } from './get-initializer';
@@ -15,38 +15,38 @@ import { getInitializer } from './get-initializer';
  * @param mainPath
  */
 export function getAppComponent(tree: Tree, mainPath: string): ClassDeclaration | undefined {
-	const bootstrapApplicationFn = getBootstrapApplicationFn(mainPath);
+  const bootstrapApplicationFn = getBootstrapApplicationFn(mainPath);
 
-	if (bootstrapApplicationFn) {
-		const component = bootstrapApplicationFn.getArguments()[0];
+  if (bootstrapApplicationFn) {
+    const component = bootstrapApplicationFn.getArguments()[0];
 
-		if (Node.isIdentifier(component)) {
-			const componentDeclaration = component.getDefinitionNodes()[0];
+    if (Node.isIdentifier(component)) {
+      const componentDeclaration = component.getDefinitionNodes()[0];
 
-			if (Node.isClassDeclaration(componentDeclaration)) {
-				return componentDeclaration;
-			}
-		}
-	} else {
-		const mainModule: ClassDeclaration = getMainModule(mainPath);
-		const mainInitializer: Expression | undefined = getInitializer(
-			mainModule,
-			'NgModule',
-			'declarations',
-		);
+      if (Node.isClassDeclaration(componentDeclaration)) {
+        return componentDeclaration;
+      }
+    }
+  } else {
+    const mainModule: ClassDeclaration = getMainModule(mainPath)!;
+    const mainInitializer: Expression | undefined = getInitializer(
+      mainModule,
+      'NgModule',
+      'declarations',
+    );
 
-		if (Node.isArrayLiteralExpression(mainInitializer)) {
-			const appIdentifier: Expression | undefined = mainInitializer.getElements()[0];
+    if (Node.isArrayLiteralExpression(mainInitializer)) {
+      const appIdentifier: Expression | undefined = mainInitializer.getElements()[0];
 
-			if (Node.isIdentifier(appIdentifier)) {
-				const appComponent: Node | undefined = appIdentifier.getDefinitionNodes()[0];
+      if (Node.isIdentifier(appIdentifier)) {
+        const appComponent: Node | undefined = appIdentifier.getDefinitionNodes()[0];
 
-				if (Node.isClassDeclaration(appComponent)) {
-					return appComponent;
-				}
-			}
-		}
-	}
+        if (Node.isClassDeclaration(appComponent)) {
+          return appComponent;
+        }
+      }
+    }
+  }
 
-	return undefined;
+  return undefined;
 }
