4afa9540b99e1ca123d8eb37932cee63b37bc9fe
Daniel Harvey <daniel@harves.net>
2025-01-24 01:34:20 +0800

    fix(builder): Add Node.isJSDocable() test for firstNodeWithComment()
    With the addition of support for TypeAliasDeclaration's this method
    is being called with Node's which do not have JSDoc.
    
    Related: Add test for undefined in getJsDoc*() methods.

--- libs/builder/helpers/typescript/first-node-with-comment.ts
+++ libs/builder/helpers/typescript/first-node-with-comment.ts
@@ -1,9 +1,9 @@
-import { JSDocableNode } from 'ts-morph';
+import { Node } from 'ts-morph';
 
 /**
  * Returns first node that has JSDoc comment, will return first node, if there are no comment nodes
  * @param nodes
  */
-export function firstNodeWithComment<T extends JSDocableNode>(nodes: T[]): T | undefined {
-  return nodes.find((n: JSDocableNode) => n.getJsDocs().length) ?? nodes[0];
+export function firstNodeWithComment<T extends Node>(nodes: T[]): T | undefined {
+  return nodes.find((n: Node) => Node.isJSDocable(n) && n.getJsDocs().length) ?? nodes[0];
 }
