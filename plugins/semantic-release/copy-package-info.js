21a85a6856271d27ad0ce25c98df51f9d8bbaad8
Aleksandr Skoropad <alex@skoropad.dev>
2025-06-15 17:30:47 +0200

    fix: custom sematic-release plugins

--- plugins/semantic-release/copy-package-info.js
+++ plugins/semantic-release/copy-package-info.js
@@ -1,29 +1,29 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
 
 /* The plugin updates dependencies between packages based on the generated semantic-release version */
 
 function prepare(pluginConfig, { logger }) {
-	const originalPackageJson = JSON.parse(
-		fs.readFileSync(pluginConfig.original, { encoding: 'utf-8' }),
-	);
+  const originalPackageJson = JSON.parse(
+    fs.readFileSync(pluginConfig.original, { encoding: 'utf-8' }),
+  );
 
-	for (let packageDir of pluginConfig.packages) {
-		logger.log('Updating package.json of %s', packageDir);
+  for (let packageDir of pluginConfig.packages) {
+    logger.log('Updating package.json of %s', packageDir);
 
-		const packageJson = JSON.parse(
-			fs.readFileSync(`${path.join(packageDir, 'package.json')}`, { encoding: 'utf-8' }),
-		);
+    const packageJson = JSON.parse(
+      fs.readFileSync(`${path.join(packageDir, 'package.json')}`, { encoding: 'utf-8' }),
+    );
 
-		for (let key of pluginConfig.keys) {
-			packageJson[key] = originalPackageJson[key];
-		}
+    for (let key of pluginConfig.keys) {
+      packageJson[key] = originalPackageJson[key];
+    }
 
-		fs.writeFileSync(
-			`${path.join(packageDir, 'package.json')}`,
-			JSON.stringify(packageJson, undefined, 2),
-		);
-	}
+    fs.writeFileSync(
+      `${path.join(packageDir, 'package.json')}`,
+      JSON.stringify(packageJson, undefined, 2),
+    );
+  }
 }
 
 module.exports = { prepare };
