b4f0482c2d900e36cf96948acd4cafcebb53742a
Adrian Kokot <35179235+AdrianKokot@users.noreply.github.com>
2025-06-25 11:30:00 +0200

    fix: ng-doc-ignore-line regex was working only for one digit number (#288)
    Fixes #283

--- libs/builder/helpers/remove-lines-from-code.ts
+++ libs/builder/helpers/remove-lines-from-code.ts
@@ -1,27 +1,27 @@
 const LINE_BREAK = /\r?\n/;
-const IGNORE_COMMENT = /\n?\r?^.*((\/\/|<!--|\/\*)\s*)(ng-doc-ignore-line)(\s*([0-9]))?.*$/;
+const IGNORE_COMMENT = /\n?\r?^.*((\/\/|<!--|\/\*)\s*)(ng-doc-ignore-line)(\s*([0-9]+))?.*$/;
 
 /**
  * Remove lines from code that are marked with `ng-doc-ignore-line`
  * @param code - The code to remove lines from
  */
 export function removeLinesFromCode(code: string): string {
-	const lines = code.split(LINE_BREAK);
-	let linesToIgnore = 0;
+  const lines = code.split(LINE_BREAK);
+  let linesToIgnore = 0;
 
-	return lines
-		.reduce((lines, line) => {
-			const match = IGNORE_COMMENT.exec(line);
+  return lines
+    .reduce((lines, line) => {
+      const match = IGNORE_COMMENT.exec(line);
 
-			if (match) {
-				linesToIgnore = Math.max(linesToIgnore, parseInt(match[5] ?? '1'));
-			} else if (linesToIgnore === 0) {
-				lines.push(line);
-			} else if (linesToIgnore > 0) {
-				linesToIgnore--;
-			}
+      if (match) {
+        linesToIgnore = Math.max(linesToIgnore, parseInt(match[5] ?? '1'));
+      } else if (linesToIgnore === 0) {
+        lines.push(line);
+      } else if (linesToIgnore > 0) {
+        linesToIgnore--;
+      }
 
-			return lines;
-		}, [] as string[])
-		.join('\n');
+      return lines;
+    }, [] as string[])
+    .join('\n');
 }
