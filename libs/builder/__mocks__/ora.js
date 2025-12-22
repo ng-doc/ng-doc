537cd4ddf380050eeb55c6cb85b384120d42ab40
Aleksandr Skoropad <alex@skoropad.dev>
2025-12-22 17:59:43 +0100

    test: add mocks for `ora` module in Jest

---
+++ libs/builder/__mocks__/ora.js
@@ -0,0 +1,13 @@
+// Mock for ora module to avoid ES module issues in Jest
+module.exports = function ora(options) {
+  return {
+    start: () => ({ succeed: () => {}, fail: () => {}, stop: () => {} }),
+    succeed: () => {},
+    fail: () => {},
+    stop: () => {},
+    text: '',
+    color: 'blue',
+  };
+};
+
+module.exports.default = module.exports;
